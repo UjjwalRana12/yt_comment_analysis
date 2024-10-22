2b73f6d6587eeb0e17370f596c37eaedfd80253f
Ujjwal <Ujjwal22154010@akgec.ac.in>
2024-10-22 10:36:11 +0530

    port hosted

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ async def analyze_comments(data: YouTubeURLInput):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8001))  
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=port)
